206cfabfb2846ee8168e231408e37422d6b8c7e1
marcelo-sl <mar.sousa2061@gmail.com>
2022-01-27 21:13:55 -0300

    Project finished

---
+++ src/main/java/com/devsuperior/projetofinalcap1/ProjetoFinalCap1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projetofinalcap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalCap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalCap1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/projetofinalcap1/ProjetoFinalCap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projetofinalcap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalCap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
